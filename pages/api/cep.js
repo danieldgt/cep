fb45fe886b37d31fe43e750018f1a496cd2b8993
furukawa <danieldgt@gmail.com>
2022-05-05 14:31:01 -0300

    teste env

--- pages/api/cep.js
+++ pages/api/cep.js
@@ -2,8 +2,9 @@ export default function handler(req, res) {
 
   let cep = req.query.cep;
 
+  let valorEnv = process.env.TEST_ENV;
 
   res.statusCode = 200;
   res.setHeader('Content-Type', 'application/json');
-  res.json({ name: 'John Doe', cep: cep });
+  res.json({ name: 'John Doe', cep: cep, env: valorEnv});
 }
