b38e949968b0741dea7caf7d1d1b31dfaefc133b
furukawa <danieldgt@gmail.com>
2022-05-05 10:47:25 -0300

    primeira versão do cep

---
+++ pages/index.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+
+function cep() {
+    return (
+        <div>
+            <h1>CEP -GERA 2</h1>
+            <Contador />
+        </div>
+    )
+}
+
+function Contador() {
+    const [ contador, setContador ] = useState(1);
+
+    function adicionarContador() {
+        setContador(contador + 1);
+    }
+
+    return (
+        <div>
+            <div>{contador}</div>
+            <button onClick={adicionarContador}>Adicionar</button>
+        </div>
+    )
+}
+
+export default cep
